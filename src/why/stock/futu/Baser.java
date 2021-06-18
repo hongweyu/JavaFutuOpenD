76b8363564fd99bdfdd7a9b29c16aad7b2d2ca5d
hongweyu <49824537+hongweyu@users.noreply.github.com>
2021-06-19 06:59:15 +0800

    Update Baser.java

--- src/why/stock/futu/Baser.java
+++ src/why/stock/futu/Baser.java
@@ -25,6 +25,65 @@ public abstract class Baser {
 	    return new String(hexChars);
 	}
 	
+	public static String octal2String(String octal, String split){
+		Vector<String> strs = Util.strSplit(octal, split, true, false, new Vector<String>());
+		for(int i=1, n=strs.size(); i<n; i++){
+			String str = strs.get(i);
+			int off = str.indexOf('"');
+			strs.set(i, transfromOctalToString("\\" + str.substring(0, off)) + str.substring(off+1));
+		}
+		return Util.join(strs, split.substring(0, split.length()-2));
+	}
+	
+	/**将包含八进制数据的字符串转换为汉字字符串。
+	* 例如将 dynamicMsg: \344\270\215\350\203\27510 转换为 dynamicMsg: 不能10*/
+	public static String transfromOctalToString(String dataStr) {
+//		if (! dataStr.contains("\\")) {
+//			return dataStr;
+//		}
+		//正常字符
+		StringBuilder buffer = new StringBuilder();
+		//八进制的内容，转成十六进制缓存
+		StringBuilder hexBuffer = new StringBuilder();
+		for (int i = 0; i < dataStr.length(); i ++) {
+			char c = dataStr.charAt(i);
+			if (c != '\\') {
+				buffer.append(c);
+			}
+			//反斜杠往后3个为一组，组成了一个八进制数。例如\20710,其实是207组成了一个八进制数
+			else {
+				//将八进制转换为十进制，再转换为十六进制
+				String hex = Integer.toHexString((Integer.valueOf(dataStr.substring(i+1, i+4), 8)));
+				i += 3;
+				//先缓存住，直到凑够三个字节
+				hexBuffer.append(hex);
+				//utf8编码中，三个字节为一个汉字
+				if (hexBuffer.length() == 6) {
+					//凑够三个字节了，转成汉字后放入oldBuffer中
+					buffer.append(hexStr2Str(hexBuffer.toString()));
+					//凑够一个汉字了，清空缓存
+					hexBuffer = new StringBuilder();
+				}
+			}
+		}
+		return buffer.toString();
+	}
+	/**
+	* 十六进制转换字符串
+	*/
+	private static String hexStr2Str(String hexStr) {
+		String str = "0123456789abcdef";
+		char[] hexs = hexStr.toCharArray();
+		byte[] bytes = new byte[hexStr.length() / 2];
+		int n;
+		for (int i = 0; i < bytes.length; i++) {
+			n = str.indexOf(hexs[2 * i]) * 16;
+			n += str.indexOf(hexs[2 * i + 1]);
+			bytes[i] = (byte) (n & 0xff);
+		}
+		return new String(bytes, StandardCharsets.UTF_8);
+	}
+	
 	public static final int bigEndian2LittleEndian32(int x) {
 		return (x & 0xFF) << 24 | (0xFF & x >> 8) << 16 | (0xFF & x >> 16) << 8 | (0xFF & x >> 24);
 	}
